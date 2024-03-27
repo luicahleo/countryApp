e083bd5db265415088ae8cdb75858d77af7d46d4
luisr <luisr@DESKTOP-4NLA5R0>
2024-03-27 17:18:56 +0100

    first commit

---
+++ src/app/shared/pages/homePage/homePage.component.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-home-page',
+  templateUrl: './homePage.component.html',
+  styles: [],
+})
+export class HomePageComponent {}
